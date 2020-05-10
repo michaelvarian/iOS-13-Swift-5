ef5164a70b36e77a9f4a81491b8e1de122e36f44
Michael Varian Sutanto <50610171+michaelvarian@users.noreply.github.com>
2020-05-11 03:57:55 +0700

    I am Poor Project for Section 3

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Michael Varian Sutanto on 11/05/20.
+//  Copyright © 2020 Michael Varian Sutanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
